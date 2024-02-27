e95f8a7c3cfa33bf1daf8181a8e587c3bd27d698
desarrollowc <desarrollo1@whitecloud.com.co>
2024-02-27 18:01:23 -0500

    handle click

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,8 @@ const emojiList = [..."💔😫🙋👨😈👽🫡💋"];
 
 function App() {
   const [memoBlockBarajados, setMemoBlockBarajados] = useState([])
-
+  const [animating, setAnimating] = useState(false)
+  const [setselectMemoBlock, setSetselectMemoBlock] = useState(null)
   useEffect(() => {
     const barajadoEmojiList = barajarArray([...emojiList, ...emojiList])
     setMemoBlockBarajados(barajadoEmojiList.map( (emoji, i ) =>({
@@ -19,15 +20,21 @@ function App() {
 
 
   const barajarArray = (a) =>{
-
-    for(let i = 8; i > 0; i--){
+    for(let i = 7; i > 0; i--){
       //usamos math.random para poder usar los numero randos, usamos el floor, por el motivo que nos pueded arrojar numero decimales, utilizando la sintaxis de desestructuración de arrays de JavaScript.
       const j = Math.floor( Math.random() * (i+1));
       [a[i], a[j]] = [a[j], a[i]];
-
     }
-
     return a;
+  }
+
+  //creamos la funcion para cuando se presione click en las cartas
+  const handleMemoClick = (memoBlock) =>{
+    //cuando se presione el clikc, la propiedad cambiar el valor filpped
+    const MemoBlockInvertido = {...memoBlock, flipped: true}
+    let memobloquesbarajadosCopy = [...memoBlockBarajados]
+    
+    setMemoBlockBarajados()
   }
 
   return (
--- src/components/MemoBlock.jsx
+++ src/components/MemoBlock.jsx
@@ -7,8 +7,9 @@ export const MemoBlock = ({memoBlock}) => {
   return (
     <>
       <div className="memo-block">
-        <div>
-          <div className='memo-block-fromt'>
+        <div className={`memo-block-inner ${memoBlock.flipped && "memo-block-flipped"}`}>
+          <div className='memo-block-front'>
+            
          </div>
           <div className='memo-block-back'>
             {memoBlock.emoji}
